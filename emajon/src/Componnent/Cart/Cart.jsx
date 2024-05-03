493985508cb77d3eb956f7fa3b42f8daa5bf6ce4
fecrakib <rakibislam132575@gmail.com>
2024-05-03 09:32:27 +0600

    caltulation complete

--- emajon/src/Componnent/Cart/Cart.jsx
+++ emajon/src/Componnent/Cart/Cart.jsx
@@ -3,19 +3,22 @@
 function Cart({cart}) {
     console.log(cart)
     let totalPrice = 0;
+    let shipping=0;
     for (const product of cart) {
         totalPrice +=parseFloat(product.price);
+        shipping=shipping+parseFloat(product.shipping)
     }
     // console.log(totalPrice)
-    console.log(totalPrice)
+   let taxt=(totalPrice*7)/100;
+   let grandTotal=totalPrice+shipping+taxt;
   return (
     <div className="bg-orange-400 p-4">   
   <h1>Order summary </h1> 
   <p>Selected product:{cart.length}</p>
   <p>Total Price:${totalPrice}</p>
-  <p>Total Shipping:</p>
-  <p>Tax:</p>
-  <h4>Grand Total:</h4>
+  <p> Shipping:${shipping}</p>
+  <p>Tax:${taxt}</p>
+  <h4>Grand Total:{grandTotal}</h4>
     </div>
   )
 }
