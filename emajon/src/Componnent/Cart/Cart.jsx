48fbc951f88f50e7a2a1309ba423ffbfe9bf6314
fecrakib <rakibislam132575@gmail.com>
2024-05-04 18:09:36 +0600

    quantity handle complete

--- emajon/src/Componnent/Cart/Cart.jsx
+++ emajon/src/Componnent/Cart/Cart.jsx
@@ -1,20 +1,24 @@
 
 
 function Cart({cart}) {
-   console.log(cart);
+   
     let totalPrice = 0;
     let shipping=0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice +=parseFloat(product.price);
-        shipping +=parseFloat(product.shipping)
+      product.quantity=product.quantity ||1;
+        totalPrice +=parseFloat(product.price*product.quantity);
+        shipping +=parseFloat(product.shipping);
+        quantity += parseFloat(product.quantity);
     }
+    console.log("quantiy",quantity);
     // console.log(totalPrice)
    let tax=parseFloat(((totalPrice*7)/100).toFixed(2));
    let grandTotal=totalPrice + shipping + tax;
   return (
     <div className="bg-orange-400 p-4">   
   <h1>Order summary </h1> 
-  <p>Selected product:{cart.length}</p>
+  <p>Selected product:{quantity}</p>
   <p>Total Price:${totalPrice}</p>
   <p> Shipping:${shipping}</p>
   <p>Tax:${tax}</p>
